a845c5f5386adf62063b80bbb374f2b55d2abab2
isaacscardoso <isaacscardoso@protonmail.com>
2025-03-11 20:17:42 -0300

    refactor(T-001): Renames the ‘user’ resource to ‘users’.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
-import { UserModule } from 'src/user/user.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
