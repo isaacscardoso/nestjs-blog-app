6b71698089fc15c20377dbbfde613c4899797111
isaacscardoso <isaacscardoso@protonmail.com>
2025-03-11 20:37:46 -0300

    feature(T-001): Add prefix to api routes and enable versioning.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
+import { VersioningType } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
